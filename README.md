4b0baf069ca3e59b50245ce4037d202be0df5503
Tyler Davis <coding@tydavis.dev>
2023-12-12 16:12:39 -0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# final-exam-calculator
+# Final Exam Calculator
+A quick little python program to give you let you know what you need to score on the final to achieve the grade that you want in the class.
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+desiredScore = float(input("What percent do you need in the class?\n"))
+finalWeighting = float(input("How much weighting does the final have?\n"))
+currentScore = float(input("What is your current grade in the class?\n"))
 
+finalScore = (100*desiredScore - ((100-finalWeighting)*(currentScore)))/finalWeighting
+
+finalScore = str(finalScore)
+desiredScore = str(desiredScore)
+
+print("You need to acheive a " + finalScore + "% on the final to get a " + desiredScore + "% in the class.")
